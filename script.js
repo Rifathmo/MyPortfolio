6522dbc21e1c1fa80e9f1ad5f475d45234203bb0
rifathmohamed27 <mrifath542@gamil.com>
2024-08-20 12:53:26 +0530

    Updated

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["UI UX Designer","Web Desginer"],
+        strings: ["UI UX Designer","Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["UI UX Designer","Web Desginer"],
+        strings: ["UI UX Designer","Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
